c3c25958a5555cb607cce453e240bcc1626130c7
suryadev78 <pandeysuryadev00@gmail.com>
2025-10-01 00:37:47 +0530

    added base tele bot setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import TelegramBot from "node-telegram-bot-api";
+const token = process.env.TELEGRAM_TOKEN as string;
+const bot = new TelegramBot(token, {polling: true});  // a bot that uses "long polling"
 
-const app = express();
+console.log("bot started");
+// Matches "/start" with optional text
+bot.onText(/\/start/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId,"Welcome to the suryadev78_bot, it's a bot built as a personal project that lets you trade SOL");
+  console.log("sent welcome message");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-}); 
+
